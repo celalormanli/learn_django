9bac2ec7d2f003df29eda517a7f5e7d28bc19cd4
Celal ORMANLI <ormanlicelal@gmail.com>
2020-12-09 02:10:02 +0300

    Database

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
-posts=[
-    {
-        'author':'friday',
-        'title':'Test title',
-        'content':'Test Content',
-        'date_posted':'January 06, 2020'
-    },
-    {
-        'author':'jarvis',
-        'title':'Test title2',
-        'content':'Test Content2',
-        'date_posted':'January 08, 2020'
-    }
-]
+from .models import Post
 
 def home(request):
     context={
-        'posts':posts
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html',context)
 
