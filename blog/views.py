4c5b2fe2745aa88e81a4565fa842d81e9c4abbfd
Celal ORMANLI <ormanlicelal@gmail.com>
2020-12-09 01:04:54 +0300

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts=[
+    {
+        'author':'friday',
+        'title':'Test title',
+        'content':'Test Content',
+        'date_posted':'January 06, 2020'
+    },
+    {
+        'author':'jarvis',
+        'title':'Test title2',
+        'content':'Test Content2',
+        'date_posted':'January 08, 2020'
+    }
+]
+
 def home(request):
-    return HttpResponse('<h1>Blog Home</h>')
+    context={
+        'posts':posts
+    }
+    return render(request,'blog/home.html',context)
 
 def about(request):
-    return HttpResponse('<h1>About Page</h>')
+    return render(request,'blog/about.html', {'title':'About'})
